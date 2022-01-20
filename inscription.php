0e83b519766a20776dfbaf91fb9376c086ecc73c
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-01-20 14:56:27 +0100

    initial commit

---
+++ inscription.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> l'université de Tours</title>
+<link href="style/style.css" rel="stylesheet" type="text/css" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+</head>
+
+<body>
+<div>
+vide
+</div>
+
+<script id="__bs_script__">//<![CDATA[
+    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.27.7'><\/script>".replace("HOST", location.hostname));
+//]]></script>
+</body>
+
+</html>
